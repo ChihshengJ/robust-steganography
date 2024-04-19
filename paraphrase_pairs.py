3813dcb140e415cfecdb9e3a4dfbd3f8042896b4
Neil Perry <neilperry1964@gmail.com>
2024-04-19 18:58:19 -0500

    initial set of paraphrase pairs to check embedding closeness

---
+++ paraphrase_pairs.py
@@ -0,0 +1,47 @@
+originals = [
+    "A basso porto (At the Lower Harbour) is an opera in three acts by the Italian composer Niccola Spinelli. The opera sets an Italian-language libretto by Eugene Checchi, based on Goffredo Cognetti's 1889 play O voto. It premiered to critical success at the Cologne Opera on April 18, 1894, sung in a German translation by Ludwig Hartmann and Otto Hess. This watercolour illustration depicts the set design by Riccardo Salvadori for act 1 of the opera's premiere. A basso porto is set in the slums of Naples, and Spinelli included mandolins and guitars in his orchestral score.",
+
+"I haven't seen you in so long! How have you been? Are you free for coffee on Friday to catch up?",
+
+"Since the late 20th century, Naples has had significant economic growth, helped by the construction of the Centro Direzionale business district and an advanced transportation network, which includes the Alta Velocità high-speed rail link to Rome and Salerno and an expanded subway network. Naples is the third-largest urban economy in Italy by GDP, after Milan and Rome. The Port of Naples is one of the most important in Europe. In addition to commercial activities, it is home to NATO's Allied Joint Force Command Naples and of the Parliamentary Assembly of the Mediterranean.",
+
+"NATO formed with twelve founding members and has added new members ten times, most recently when Sweden joined the alliance on 7 March 2024.[14] In addition, NATO currently recognizes Bosnia and Herzegovina, Georgia, and Ukraine as aspiring members. Enlargement has led to tensions with non-member Russia, one of the 18 additional countries participating in NATO's Partnership for Peace programme. Another nineteen countries are involved in institutionalized dialogue programmes with NATO.",
+
+"Washington, D.C., anchors the southern end of the Northeast megalopolis, one of the nation's largest and most influential cultural, political, and economic regions. As the seat of the U.S. federal government and several international organizations, the city is an important world political capital. The city had 20.7 million domestic visitors and 1.2 million international visitors, ranking seventh among U.S. cities as of 2022.",
+
+"British political philosopher John Locke following the Glorious Revolution of 1688 was a major influence expanding on the contract theory of government advanced by Thomas Hobbes, his contemporary. Locke advanced the principle of consent of the governed in his Two Treatises of Government. Government's duty under a social contract among the sovereign people was to serve the people by protecting their rights. These basic rights were life, liberty and property.",
+
+"The earliest commercial Japanese animation dates to 1917. A characteristic art style emerged in the 1960s with the works of cartoonist Osamu Tezuka and spread in following decades, developing a large domestic audience. Anime is distributed theatrically, through television broadcasts, directly to home media, and over the Internet. In addition to original works, anime are often adaptations of Japanese comics (manga), light novels, or video games. It is classified into numerous genres targeting various broad and niche audiences.",
+
+"In October 2021, Toei Animation announced that they had signed a strategic partnership with the South Korean entertainment conglomerate CJ ENM.",
+
+"I think that Naruto is my favorite anime but it's not the best anime. That's probably something like Fullmetal Alchemist or Hunter X Hunter (even though it was never finished).",
+
+"This week has been so busy. I've been interviewing students back to back all day and my schedule has been packed. How about you?",
+
+"Where would you want to go on vacation? It has to be somewhere you've never been. I think it'd be Dubai for me.",
+]
+
+paraphrases = [
+        "\"A basso porto (At the Lower Harbour)\" is a three-act opera by Niccola Spinelli, featuring a libretto in Italian by Eugene Checchi that adapts Goffredo Cognetti's 1889 drama, O voto. Its debut, performed in German by translators Ludwig Hartmann and Otto Hess, occurred at the Cologne Opera on April 18, 1894, and was met with acclaim. This watercolor represents Riccardo Salvadori's set design for the first act. Set against the backdrop of Naples' impoverished areas, the score of \"A basso porto\" is enriched by the inclusion of mandolins and guitars.",
+
+    "It’s been ages since we last met! How are things with you? Do you have time to meet for a coffee on Friday and catch up?",
+
+    "Naples has experienced notable economic development since the late 20th century, bolstered by the establishment of the Centro Direzionale business district and a sophisticated transport system, including the high-speed Alta Velocità rail connecting it to Rome and Salerno, alongside an enhanced metro network. Naples ranks as Italy's third largest city economy, trailing only Milan and Rome, and hosts a critical European port. The city also houses NATO's Allied Joint Force Command Naples and the Parliamentary Assembly of the Mediterranean.",
+
+    "NATO has expanded since its creation, most recently including Sweden as a member on March 7, 2024. The alliance also lists Bosnia and Herzegovina, Georgia, and Ukraine as potential members. NATO's expansion has occasionally increased tensions with Russia, which is involved in NATO's Partnership for Peace program, alongside eighteen other countries. Additionally, nineteen other nations engage in structured dialogue programs with NATO.",
+
+    "Washington, D.C. marks the southern tip of the Northeast megalopolis and plays a significant role as a major U.S. cultural, political, and economic hub. It serves as the capital for the U.S. federal government and various global institutions, attracting 20.7 million domestic and 1.2 million international visitors in 2022, making it the seventh most visited city in the United States.",
+
+    "British philosopher John Locke was instrumental after the Glorious Revolution in 1688 in furthering the theory of governance based on social contract, an idea earlier touched upon by his contemporary Thomas Hobbes. Locke's Two Treatises of Government emphasized the consent of the governed and the responsibility of government to uphold the people's rights, primarily to life, liberty, and property.",
+
+    "Japanese commercial animation, dating back to 1917, evolved into a distinctive style in the 1960s through cartoonist Osamu Tezuka and has since gained a significant following in Japan. Anime, often based on manga, light novels, or video games, is distributed through cinema, TV, home media, and online platforms, catering to a variety of audiences across different genres.",
+
+    "In October 2021, Toei Animation formed a strategic partnership with South Korean media giant CJ ENM.",
+
+    "Naruto might be my favorite anime, though I wouldn't say it's the greatest; that honor could go to Fullmetal Alchemist or Hunter X Hunter, despite the latter being incomplete.",
+
+    "This week has been non-stop for me, filled with student interviews from morning till evening. What’s new with you?",
+
+    "If you could pick a vacation destination where you've never been, where would it be? I’m thinking Dubai sounds like an exciting choice.",
+]
